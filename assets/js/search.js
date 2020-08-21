bc92f302abc4385472540ce5d67870f2882302f1
johnkimdy <johnkimdy@users.noreply.github.com>
2020-08-21 04:48:47 +0000

    deploy: d6b695c8758d94c246fa592436c6fe6d0ce7bba3

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/fastpagetest/assets/js/search-data.json', true);
+    request.open('GET', '/fastpagetest.github.io/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
